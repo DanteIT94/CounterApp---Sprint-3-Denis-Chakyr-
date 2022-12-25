df685d01e7f47a81c3b2d1c9255c0db40c4324d2
Denis Chakyr <ChakyrIT@gmail.com>
2022-12-25 20:53:50 +0200

    Приложение - Счетчик (Спринт 3 для Яндекс-Практикум)

--- CounterApp - Sprint 3 (Denis Chakyr)/ViewController.swift
+++ CounterApp - Sprint 3 (Denis Chakyr)/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var buttonAddElement: UIButton!
+    @IBOutlet weak var buttonDeleteElement: UIButton!
+    @IBOutlet weak var buttonResetTo0: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    var scoreCounter = 0
+    @IBAction func addOneElement(_ sender: Any) {
+        counterLabel.text = "Значение счётчика: \(scoreCounter + 1)"
+        scoreCounter += 1
+    }
+    
+    @IBAction func deleteOneElement(_ sender: Any) {
+        if scoreCounter == 0 {
+        } else {
+        counterLabel.text = "Значение счётчика: \(scoreCounter - 1)"
+        scoreCounter -= 1
+        }
+    }
+    
+    @IBAction func resetScore(_ sender: Any) {
+        scoreCounter = 0
+        counterLabel.text = "Значение счётчика: \(scoreCounter)"
+    }
 }
 
